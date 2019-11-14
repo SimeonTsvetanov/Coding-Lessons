37cb4b30bcba2a89efce90855abe233a28047580
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2019-11-15 00:19:54 +0200

    6th

---
+++ SoftUni Lessons/Python Development/Python Fundamentals September 2019/Problems And Files/6 DATA TYPES AND VARIABLES - Дата 25-ти септември, 1830 - 2130/05. Pounds to Dollars.py
@@ -0,0 +1,17 @@
+"""
+Data Types and Variables - Lab
+Check your code: https://judge.softuni.bg/Contests/Practice/Index/1721#4
+Video: https://www.youtube.com/watch?v=i2vHypjJkB4
+
+SUPyF2 D.Types and Vars Lab - 05. Pounds to Dollars
+
+Problem:
+Write a program that converts British pounds to US dollars formatted to 3th decimal point.
+1 British Pound = 1.31 Dollars
+
+Example:
+Input:	Output:
+80	    104.800
+39	    51.090
+"""
+print(f"{(int(input()) * 1.31):.3f}")
---
+++ SoftUni Lessons/Python Development/Python Fundamentals September 2019/Problems And Files/6 DATA TYPES AND VARIABLES - Дата 25-ти септември, 1830 - 2130/README.md
@@ -0,0 +1,15 @@
+<h1 align="center">DATA TYPES AND VARIABLES</h1>
+
+<hr>
+
+<blockquote>
+    <p>Дата: 25-ти септември, 18:30 - 21:30</p>
+</blockquote>
+
+<br>
+
+<p>
+    Video from the Lection: <a href="https://www.youtube.com/watch?v=i2vHypjJkB4">here</a>
+</p>
+
+<hr>
