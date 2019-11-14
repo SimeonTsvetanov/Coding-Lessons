4f89ca865bf2beed41bcd79e2fa8cccb62f6350d
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2019-11-15 00:18:05 +0200

    3 and 4 folders

---
+++ SoftUni Lessons/Python Development/Python Fundamentals September 2019/Problems And Files/4 BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS - Дата 18-ти септември, 1830 - 2130/03. Word Reverse.py
@@ -0,0 +1,23 @@
+"""
+Basic Syntax, Conditional Statements and Loops - Lab
+Check your code: https://judge.softuni.bg/Contests/Practice/Index/1718#2
+Video: https://www.youtube.com/watch?v=sP_t7cbZF7c
+
+SUPyF2 Basic - 03. Word Reverse
+
+Problem:
+Write a program that receives a single word from the user, reverses it and prints it
+
+Example:
+
+Input:
+    Python
+Output:
+    nohtyP
+
+Input:
+    banana
+Output:
+    ananab
+"""
+print(input()[::-1])
