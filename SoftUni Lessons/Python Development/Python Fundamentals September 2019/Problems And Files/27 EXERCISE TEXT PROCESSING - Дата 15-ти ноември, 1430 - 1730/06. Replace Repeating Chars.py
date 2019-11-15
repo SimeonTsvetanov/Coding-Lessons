4ca9c975fc3c0f441b380a20f119e805336426e4
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2019-11-15 15:58:23 +0200

    Adding the Solution from the Lesson

--- SoftUni Lessons/Python Development/Python Fundamentals September 2019/Problems And Files/27 EXERCISE TEXT PROCESSING - Дата 15-ти ноември, 1430 - 1730/06. Replace Repeating Chars.py
+++ SoftUni Lessons/Python Development/Python Fundamentals September 2019/Problems And Files/27 EXERCISE TEXT PROCESSING - Дата 15-ти ноември, 1430 - 1730/06. Replace Repeating Chars.py
@@ -12,6 +12,9 @@ Input	                    Output
 aaaaabbbbbcdddeeeedssaa	    abcdedsa
 qqqwerqwecccwd	            qwerqwecwd
 """
+
+# This was the initial Solution
+"""
 past_letter = ""
 for i in input():
     if len(past_letter) == 0:
@@ -22,3 +25,26 @@ for i in input():
         else:
             past_letter += i
 print(past_letter)
+"""
+
+# This is the solution in class:
+text = input()
+i = 0
+
+while i < len(text) - 1:
+    if text[i] == text[i + 1]:
+        text = text.replace(f"{text[i]}{text[i + 1]}", f"{text[i]}")
+        i -= 1
+    i += 1
+print(text)
+
+
+
+
+
+
+
+
+
+
+
