dc7cc3593c514e5ce981e48c1e46e805d1b242b9
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2019-11-30 19:57:48 +0200

    Quick update
    Adding the solution from work in class

--- SoftUni Lessons/Python Development/Python Fundamentals September 2019/Problems And Files/32 EXAM PREPARATION - Дата 27-ми ноември, 1830 - 2130/Message Decrypter.py
+++ SoftUni Lessons/Python Development/Python Fundamentals September 2019/Problems And Files/32 EXAM PREPARATION - Дата 27-ми ноември, 1830 - 2130/Message Decrypter.py
@@ -58,6 +58,34 @@ Valid message not found!
 Valid message not found!
 Valid message not found!
 """
+
+# This is the solution from work in class:
+
+"""
+import re
+
+col = int(input())
+regex = r"(^\$[A-Z]{1}[a-z][a-z]+\$:[ ](\[[0-9]+\]\|){3}$)|(^\%[A-Z]{1}[a-z][a-z]+\%:[ ](\[[0-9]+\]\|){3}$)"
+
+for i in range(col):
+    a = input()
+    match = re.findall(regex, a)
+    if match:
+        a = a.split(": ")
+        word = a[0]
+        word = word[1:-1]
+        asc = a[1].split("|")
+        asc.pop()
+        y = ""
+        for x in asc:
+            x = int(x[1:-1])
+            y += chr(x)
+        print(f"{word}: {y}")
+    else:
+        print("Valid message not found!")
+"""
+
+# And this is my solution
 import re
 
 regex = r"^(?P<tag>[$|%])(?P<name>[A-Z][a-z][a-z]+)(?P=tag):[ ](\[(?P<one>\d+)\]\|)(\[(?P<two>\d+)\]\|)(\[(?P<three>\d+)\]\|)$"
