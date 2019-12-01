1a9cce7953f970b64bb0a98d85606bcdbf265167
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2019-12-01 16:51:09 +0200

    Upload the initial Problem files
    Upload the initial Problem files without the solutions

---
+++ SoftUni Lessons/Python Development/Python Fundamentals September 2019/Problems And Files/33 EXAM PREPARATION - Дата 29-ти ноември, 1430 - 1730/Followers.py
@@ -0,0 +1,8 @@
+"""
+Exam Preparation Programming Fundamentals Final Exam Retake - 9 August 2019
+Check your code: https://judge.softuni.bg/Contests/Practice/Index/1767#2
+Name: Followers
+
+Problem:
+
+"""
---
+++ SoftUni Lessons/Python Development/Python Fundamentals September 2019/Problems And Files/33 EXAM PREPARATION - Дата 29-ти ноември, 1430 - 1730/Username.py
@@ -0,0 +1,9 @@
+"""
+Exam Preparation Programming Fundamentals Final Exam Retake - 9 August 2019
+Check your code: https://judge.softuni.bg/Contests/Practice/Index/1767#0
+
+Name: Username
+
+Problem:
+
+"""
