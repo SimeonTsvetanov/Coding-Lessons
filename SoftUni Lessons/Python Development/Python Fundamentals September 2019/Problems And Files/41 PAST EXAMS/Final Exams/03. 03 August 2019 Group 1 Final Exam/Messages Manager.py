9cd6118f357c6142be7b5c9d409bf7bb46682ca9
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2019-12-03 12:24:00 +0200

    Update the solution with Object/ classes

--- SoftUni Lessons/Python Development/Python Fundamentals September 2019/Problems And Files/41 PAST EXAMS/Final Exams/03. 03 August 2019 Group 1 Final Exam/Messages Manager.py
+++ SoftUni Lessons/Python Development/Python Fundamentals September 2019/Problems And Files/41 PAST EXAMS/Final Exams/03. 03 August 2019 Group 1 Final Exam/Messages Manager.py
@@ -162,6 +162,7 @@ for user in sorted_list:
 """
 
 # Tis is the initial solution in which I have used the normal way with Dictionary
+"""
 capacity = int(input())
 
 users = {}
@@ -202,3 +203,83 @@ print(f"Users count: {len(users)}")
 
 for user, messages in sorted(users.items(), key=lambda x: (-(x[1][1]), x[0])):
     print(f"{user} - {sum(messages)}")
+"""
+
+# And this is the solution done with Object/Classes:
+class User:
+    max_messages = 0
+    all = []
+
+    def __init__(self, name: str, sent: int, received: int):
+        self.name = name
+        self.sent = sent
+        self.received = received
+        self.count_all_messages = sent + received
+
+    @classmethod
+    def is_present(cls, name_to_check):
+        for user in User.all:
+            if user.name == name_to_check:
+                return True
+        return False
+
+    @classmethod
+    def add(cls, name_to_add: str, sent_to_add: int, received_to_add: int):
+        if not User.is_present(name_to_add):
+            User.all.append(User(name=name_to_add, sent=sent_to_add, received=received_to_add))
+
+    @classmethod
+    def message(cls, sender: str, receiver:str):
+        if User.is_present(sender) and User.is_present(receiver):
+            for user in User.all:
+                if user.name == sender:
+                    user.sent += 1
+                    user.count_all_messages += 1
+                elif user.name == receiver:
+                    user.received += 1
+                    user.count_all_messages += 1
+            User.check_capacity(sender)
+            User.check_capacity(receiver)
+
+    @classmethod
+    def check_capacity(cls, name_to_check: str):
+        for user in User.all:
+            if user.name == name_to_check:
+                if user.count_all_messages >= User.max_messages:
+                    print(f"{user.name} reached the capacity!")
+                    User.all.remove(user)
+
+    @classmethod
+    def empty(cls, name_or_all: str):
+        if name_or_all == "All":
+            User.all.clear()
+        else:
+            for user in User.all:
+                if user.name == name_or_all:
+                    User.all.remove(user)
+
+    @classmethod
+    def print_all(cls):
+        print(f"Users count: {len(User.all)}")
+        for user in sorted(User.all, key=lambda x: (-x.received, x.name)):
+            print(f"{user.name} - {user.count_all_messages}")
+        exit(0)
+
+
+def run():
+    User.max_messages = int(input())
+    while True:
+        command = input().split("=")
+        if command[0] == "Statistics":
+            print(User.print_all())
+        elif command[0] == "Add":
+            User.add(name_to_add=command[1], sent_to_add=int(command[2]), received_to_add=int(command[3]))
+        elif command[0] == "Message":
+            User.message(sender=command[1], receiver=command[2])
+        elif command[0] == "Empty":
+            User.empty(name_or_all=command[1])
+
+
+
+if __name__ == '__main__':
+    run()
