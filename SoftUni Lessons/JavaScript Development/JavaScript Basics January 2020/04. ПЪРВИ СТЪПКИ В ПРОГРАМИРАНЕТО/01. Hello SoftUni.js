f6cfed22cc5563fc56a5d0e88512ae16b84faaaf
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2020-01-12 19:53:01 +0200

    Uploading the Problems Solved

---
+++ SoftUni Lessons/JavaScript Development/JavaScript Basics January 2020/04. ПЪРВИ СТЪПКИ В ПРОГРАМИРАНЕТО/01. Hello SoftUni.js
@@ -0,0 +1,6 @@
+function hello() {
+    console.log("Hello SoftUni");
+}
+
+hello();
+
---
+++ SoftUni Lessons/JavaScript Development/JavaScript Basics January 2020/04. ПЪРВИ СТЪПКИ В ПРОГРАМИРАНЕТО/04. Rectangle Area.js
@@ -0,0 +1,8 @@
+function rectangleArea() {
+    let a = 7
+    let b = 2
+    let area = a * b
+    console.log(area)
+}
+
+rectangleArea()
