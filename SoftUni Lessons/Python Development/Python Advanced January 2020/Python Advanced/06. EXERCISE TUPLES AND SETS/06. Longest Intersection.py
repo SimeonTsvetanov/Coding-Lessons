4e458437d6dc7ad5fbbd587b89d1c8dd7169ebdc
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2020-01-20 23:48:02 +0200

    Quick fix over the names.

--- SoftUni Lessons/Python Development/Python Advanced January 2020/Python Advanced/06. EXERCISE TUPLES AND SETS/06. Longest Intersection.py
+++ SoftUni Lessons/Python Development/Python Advanced January 2020/Python Advanced/06. EXERCISE TUPLES AND SETS/06. Longest Intersection.py
@@ -2,18 +2,18 @@ longest_intersection = set()
 
 for i in range(int(input())):
     intersection_one, intersection_two = input().split("-")
-    start_int_one, end_int_one = [int(x) for x in intersection_one.split(",")]
-    start_int_two, end_int_two = [int(x) for x in intersection_two.split(",")]
+    start_set_one, end_set_one = [int(x) for x in intersection_one.split(",")]
+    start_set_two, end_set_two = [int(x) for x in intersection_two.split(",")]
 
-    current_set_int_one = set()
-    current_set_int_two = set()
+    current_set_one = set()
+    current_set_two = set()
 
-    for num in range(start_int_one, end_int_one + 1):
-        current_set_int_one.add(num)
-    for num in range(start_int_two, end_int_two + 1):
-        current_set_int_two.add(num)
+    for num in range(start_set_one, end_set_one + 1):
+        current_set_one.add(num)
+    for num in range(start_set_two, end_set_two + 1):
+        current_set_two.add(num)
 
-    current_intersection = current_set_int_one & current_set_int_two
+    current_intersection = current_set_one & current_set_two
     if len(current_intersection) >= len(longest_intersection):
         longest_intersection = current_intersection
 
