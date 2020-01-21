a1ff4b322ad1f37399d5f75847c5b57702f481bf
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2020-01-21 14:38:57 +0200

    add the solution with Dict
    add the second solution and format the problem with functions

--- SoftUni Lessons/Python Development/Python Advanced January 2020/Python Advanced/06. EXERCISE TUPLES AND SETS/04. Count Symbols.py
+++ SoftUni Lessons/Python Development/Python Advanced January 2020/Python Advanced/06. EXERCISE TUPLES AND SETS/04. Count Symbols.py
@@ -1,8 +1,26 @@
-letters_set = set()
-text = input()
+def solve_with_set():  # Solution with SET:
+    letters_set = set()
+    text = input()
 
-for letter in text:
-    letters_set.add(letter)
+    for letter in text:
+        letters_set.add(letter)
 
-for letter in sorted(letters_set):
-    print(f"{letter}: {text.count(letter)} time/s")
+    for letter in sorted(letters_set):
+        print(f"{letter}: {text.count(letter)} time/s")
+
+
+def solve_with_dictionary():  # Solution with Dictionary:
+    dict_letters = {}
+    text = input()
+    for letter in text:
+        if letter not in dict_letters:
+            dict_letters[letter] = 1
+        else:
+            dict_letters[letter] += 1
+    for letter, count in sorted(dict_letters.items()):
+        print(f"{letter}: {count} time/s")
+
+
+if __name__ == '__main__':
+    solve_with_set()
+    # solve_with_dictionary()
