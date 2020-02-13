46a6ac7c9878ff1619b85e239170eb782e18bdbf
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2020-02-13 22:25:40 +0200

    Upload the tasks solutions

--- SoftUni Lessons/Python Development/Python Advanced January 2020/Python Advanced/25. PAST EXAMS/02. Python Advanced Exam Preparation - 18 February 2020/1. Dating App.py
+++ SoftUni Lessons/Python Development/Python Advanced January 2020/Python Advanced/25. PAST EXAMS/02. Python Advanced Exam Preparation - 18 February 2020/1. Dating App.py
@@ -1,1 +1,39 @@
-# Test
+from collections import deque
+
+males = [int(male) for male in input().split()]
+females = deque([int(female) for female in input().split()])
+
+matches = 0
+
+while males and females:
+    male = males.pop()
+    female = females.popleft()
+    if male <= 0 or female <= 0:
+        if male <= 0:
+            females.appendleft(female)
+        if female <= 0:
+            males.append(male)
+    else:
+        if female % 25 == 0:
+            females.popleft()
+            males.append(male)
+        elif male % 25 == 0:
+            males.pop()
+            females.appendleft(female)
+        else:
+            if female == male:
+                matches += 1
+            else:
+                males.append(male - 2)
+
+
+print(f"Matches: {matches}")
+if males:
+    print(f"Males left: {', '.join([str(male) for male in males[::-1]])}")
+else:
+    print(f"Males left: none")
+
+if females:
+    print(f"Females left: {', '.join([str(female) for female in females])}")
+else:
+    print(f"Females left: none")
--- SoftUni Lessons/Python Development/Python Advanced January 2020/Python Advanced/25. PAST EXAMS/02. Python Advanced Exam Preparation - 18 February 2020/2. Book Worm.py
+++ SoftUni Lessons/Python Development/Python Advanced January 2020/Python Advanced/25. PAST EXAMS/02. Python Advanced Exam Preparation - 18 February 2020/2. Book Worm.py
@@ -1,1 +1,74 @@
-# Test
+string = input()
+size = int(input())
+
+matrix = []
+
+# Fill the matrix
+for row in range(size):
+    matrix.append([symbol for symbol in input()])
+
+# Grt the Player Position
+player_position = []
+for row in range(len(matrix)):
+    if player_position:
+        break
+    for col in range(len(matrix[0])):
+        if matrix[row][col] == "P":
+            player_position = [row, col]
+            break
+
+for _ in range(int(input())):
+    command = input()
+
+    if command == "up":
+        if (player_position[0] - 1) >= 0:  # Move is Valid
+            current_holder = matrix[player_position[0] - 1][player_position[1]]
+            if current_holder != "-":  # So it is some letter
+                string += current_holder
+            matrix[player_position[0]][player_position[1]] = "-"
+            matrix[player_position[0] - 1][player_position[1]] = "P"
+            player_position = [player_position[0] - 1, player_position[1]]
+        else:  # Move is invalid:
+            if string:
+                string = string[:-1]
+
+    if command == "down":
+        if (player_position[0] + 1) < len(matrix):  # Move is Valid
+            current_holder = matrix[player_position[0] + 1][player_position[1]]
+            if current_holder != "-":  # So it is some letter
+                string += current_holder
+            matrix[player_position[0]][player_position[1]] = "-"
+            matrix[player_position[0] + 1][player_position[1]] = "P"
+            player_position = [player_position[0] + 1, player_position[1]]
+        else:  # Move is invalid:
+            if string:
+                string = string[:-1]
+
+    if command == "left":
+        if player_position[1] - 1 >= 0:  # Move is Valid
+            current_holder = matrix[player_position[0]][player_position[1] - 1]
+            if current_holder != "-":  # So it is some letter
+                string += current_holder
+            matrix[player_position[0]][player_position[1]] = "-"
+            matrix[player_position[0]][player_position[1] - 1] = "P"
+            player_position = [player_position[0], player_position[1] - 1]
+        else:  # Move is invalid:
+            if string:
+                string = string[:-1]
+
+    if command == "right":
+        if player_position[1] + 1 < len(matrix[0]):  # Move is Valid
+            current_holder = matrix[player_position[0]][player_position[1] + 1]
+            if current_holder != "-":  # So it is some letter
+                string += current_holder
+            matrix[player_position[0]][player_position[1]] = "-"
+            matrix[player_position[0]][player_position[1] + 1] = "P"
+            player_position = [player_position[0], player_position[1] + 1]
+        else:  # Move is invalid:
+            if string:
+                string = string[:-1]
+
+print(string)
+# Print the matrix
+for row in matrix:
+    print(''.join(row))
