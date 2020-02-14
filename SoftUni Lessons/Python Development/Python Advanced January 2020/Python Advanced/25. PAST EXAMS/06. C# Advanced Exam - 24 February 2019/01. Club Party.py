2365c718aaa214bc717307f2ea751dc560866d64
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2020-02-14 19:00:04 +0200

    Upload the tasks solved

--- SoftUni Lessons/Python Development/Python Advanced January 2020/Python Advanced/25. PAST EXAMS/06. C# Advanced Exam - 24 February 2019/01. Club Party.py
+++ SoftUni Lessons/Python Development/Python Advanced January 2020/Python Advanced/25. PAST EXAMS/06. C# Advanced Exam - 24 February 2019/01. Club Party.py
@@ -1,1 +1,22 @@
-# TODO Not Yet Solved of Uploaded
+from collections import deque
+
+halls_maximum_capacity = int(input())
+
+line = [x for x in input().split()]
+party = deque([])
+taken_guests = []
+
+while line:
+    x = line.pop()
+    if x.isalpha():
+        party.append(x)
+        continue
+    if not party:
+        continue
+    new_guests = int(x)
+    if halls_maximum_capacity >= sum(taken_guests) + new_guests:
+        taken_guests.append(new_guests)
+    else:
+        print(f"{party.popleft()} -> {', '.join([str(guests) for guests in taken_guests])}")
+        taken_guests = []
+        line.append(x)
--- SoftUni Lessons/Python Development/Python Advanced January 2020/Python Advanced/25. PAST EXAMS/06. C# Advanced Exam - 24 February 2019/02. Tron Racers.py
+++ SoftUni Lessons/Python Development/Python Advanced January 2020/Python Advanced/25. PAST EXAMS/06. C# Advanced Exam - 24 February 2019/02. Tron Racers.py
@@ -1,1 +1,73 @@
-# TODO Not Yet Solved of Uploaded
+matrix = []
+player_one_position = []
+player_two_position = []
+
+# This are the delta changes
+moves = {
+    "up": [-1, 0],
+    "down": [1, 0],
+    "left": [0, -1],
+    "right": [0, 1]
+}
+
+for row in range(int(input())):  # Get the matrix, first and second player coordinates
+    line = input()
+    matrix.append([x for x in line])  # Fill the matrix
+    if "f" in line:
+        player_one_position = [row, line.index("f")]  # Create the initial position of player one
+    if "s" in line:
+        player_two_position = [row, line.index("s")]  # Create the initial position of player two
+
+
+def change_position_if_out(player: list):
+    """
+    This Function will check if players next position will be still in the matrix.
+    Ann if is out of the matrix will change it to the other side instead.
+    :param player: The player we want to check and/or change
+    :return: the old or new coordinates of the player
+    """
+    if 0 <= player[0] < len(matrix):
+        if 0 <= player[1] < len(matrix[0]):
+            return player
+        else:
+            if player[1] < 0:
+                return [player[0], len(matrix[0]) - 1]
+            else:
+                return [player[0], 0]
+    else:
+        if player[0] < 0:
+            return [len(matrix) - 1, player[1]]
+        else:
+            return [0, player[1]]
+
+
+crash = False
+while not crash:
+    x = input().split()
+
+    # First player next move + we will check if the move is possible and if not we will fix it:
+    one_next_move = \
+        change_position_if_out([moves[x[0]][0] + player_one_position[0], moves[x[0]][1] + player_one_position[1]])
+
+    # Second player next move + we will check if the move is possible and if not we will fix it:
+    two_next_move = \
+        change_position_if_out([moves[x[1]][0] + player_two_position[0], moves[x[1]][1] + player_two_position[1]])
+
+    # Now lets check if player ONE next is cell if free and we can step on it:
+    if matrix[one_next_move[0]][one_next_move[1]] in ["*", "f"]:
+        matrix[one_next_move[0]][one_next_move[1]] = "f"
+        player_one_position = [one_next_move[0], one_next_move[1]]
+    else:
+        matrix[one_next_move[0]][one_next_move[1]] = "x"
+        crash = True
+        continue
+
+    # Now lets check if player TWO next is cell if free and we can step on it:
+    if matrix[two_next_move[0]][two_next_move[1]] in ["*", "s"]:
+        matrix[two_next_move[0]][two_next_move[1]] = "s"
+        player_two_position = [two_next_move[0], two_next_move[1]]
+    else:
+        matrix[two_next_move[0]][two_next_move[1]] = "x"
+        crash = True
+
+[print(''.join(line)) for line in matrix]  # Print the matrix
