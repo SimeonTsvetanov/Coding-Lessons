32183496bb333392946e12129f7f633eaa26ff87
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2020-02-14 13:25:30 +0200

    Upload the tasks solved.

--- SoftUni Lessons/Python Development/Python Advanced January 2020/Python Advanced/25. PAST EXAMS/05. C# Advanced Exam - 23 June 2019/01. Spaceship Crafting.py
+++ SoftUni Lessons/Python Development/Python Advanced January 2020/Python Advanced/25. PAST EXAMS/05. C# Advanced Exam - 23 June 2019/01. Spaceship Crafting.py
@@ -1,1 +1,41 @@
-# TODO Not Yet Solved of Uploaded
+from collections import deque
+
+chemical_liquids = deque([int(x) for x in input().split()])
+physical_items = [int(x) for x in input().split()]
+
+base_materials = {
+    25: "Glass",
+    50: "Aluminium",
+    75: "Lithium",
+    100: "Carbon fiber"
+}
+
+collected_materials = {
+    "Aluminium": 0,
+    "Carbon fiber": 0,
+    "Glass": 0,
+    "Lithium": 0
+}
+
+while chemical_liquids and physical_items:
+    liquid = chemical_liquids.popleft()
+    item = physical_items.pop()
+    if (liquid + item) in base_materials:
+        collected_materials[base_materials[liquid + item]] += 1
+    else:
+        physical_items.append(item + 3)
+
+if all(x > 0 for x in collected_materials.values()):
+    print(f"Wohoo! You succeeded in building the spaceship!")
+else:
+    print(f"Ugh, what a pity! You didn't have enough materials to build the spaceship.")
+if not chemical_liquids:
+    print(f"Liquids left: none")
+else:
+    print(f"Liquids left: {', '.join([str(liquid) for liquid in chemical_liquids])}")
+if not physical_items:
+    print(f"Physical items left: none")
+else:
+    print(f"Physical items left: {', '.join([str(item) for item in physical_items[::-1]])}")
+for advanced_item, count in collected_materials.items():
+    print(f"{advanced_item}: {count}")
--- SoftUni Lessons/Python Development/Python Advanced January 2020/Python Advanced/25. PAST EXAMS/05. C# Advanced Exam - 23 June 2019/03. Space Station Establishment.py
+++ SoftUni Lessons/Python Development/Python Advanced January 2020/Python Advanced/25. PAST EXAMS/05. C# Advanced Exam - 23 June 2019/03. Space Station Establishment.py
@@ -1,1 +1,66 @@
-# TODO Not Yet Solved of Uploaded
+galaxy = [[x for x in input()] for _ in range(int(input()))]  # Create the Galaxy
+
+steven_position = []  # Lets get Stephan Start position and the Black Holes if any:
+black_holes = [[], []]  # I'am storing the black holes in matrix :D
+for row in range(len(galaxy)):
+    for col in range(len(galaxy[row])):
+        if galaxy[row][col] == "S":
+            steven_position = [row, col]
+        if galaxy[row][col] == "O":
+            if not black_holes[0]:
+                black_holes[0] = [row, col]
+            else:
+                black_holes[1] = [row, col]
+
+directions = {
+    "up": [-1, 0],
+    "down": [1, 0],
+    "left": [0, -1],
+    "right": [0, 1]
+}
+
+star_power = 0
+out_of_space = False
+while star_power < 50 and not out_of_space:
+    move = directions[input()]
+    if (0 <= (steven_position[0] + move[0]) < len(galaxy)) and (0 <= (steven_position[1] + move[1]) < len(galaxy)):
+        # Then the move is possible
+
+        # First Check for the Black Hole:
+        if galaxy[steven_position[0] + move[0]][steven_position[1] + move[1]] == "O":  # He goes in the black hole:
+            for hole in range(len(black_holes)):
+                if (black_holes[hole][0] == (steven_position[0] + move[0])) \
+                        and (black_holes[hole][1] == (steven_position[1] + move[1])):
+                    black_holes.pop(hole)
+                    galaxy[steven_position[0]][steven_position[1]] = "-"
+                    galaxy[steven_position[0] + move[0]][steven_position[1] + move[1]] = "-"
+                    galaxy[black_holes[0][0]][black_holes[0][1]] = "S"
+                    steven_position = [black_holes[0][0], black_holes[0][1]]
+                    break
+
+        # And then check for the numbers:
+        elif galaxy[steven_position[0] + move[0]][steven_position[1] + move[1]] != "-":
+            star_power += int(galaxy[steven_position[0] + move[0]][steven_position[1] + move[1]])
+            galaxy[steven_position[0]][steven_position[1]] = "-"
+            galaxy[steven_position[0] + move[0]][steven_position[1] + move[1]] = "S"
+            steven_position = [steven_position[0] + move[0], steven_position[1] + move[1]]
+
+        # And if its not a soldier or Helen so it's just a empty cell:
+        else:
+            galaxy[steven_position[0]][steven_position[1]] = "-"
+            galaxy[steven_position[0] + move[0]][steven_position[1] + move[1]] = "S"
+            steven_position = [steven_position[0] + move[0], steven_position[1] + move[1]]
+
+    else:
+        # The move is impossible and he flies out of the galaxy
+        out_of_space = True
+        galaxy[steven_position[0]][steven_position[1]] = "-"
+        break
+
+if out_of_space:
+    print(f"Bad news, the spaceship went to the void.")
+else:
+    # star_power >= 50:
+    print(f"Good news! Stephen succeeded in collecting enough star power!")
+print(f"Star power collected: {star_power}")
+[print(''.join(row)) for row in galaxy]  # Print the galaxy
