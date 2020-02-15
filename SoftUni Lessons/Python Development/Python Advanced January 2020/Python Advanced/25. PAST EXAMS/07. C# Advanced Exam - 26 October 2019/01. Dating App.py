a2f4f5a6f96999b4bc31556e47dc1cc480651052
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2020-02-15 23:46:38 +0200

    Upload the Problems Solved

--- SoftUni Lessons/Python Development/Python Advanced January 2020/Python Advanced/25. PAST EXAMS/07. C# Advanced Exam - 26 October 2019/01. Dating App.py
+++ SoftUni Lessons/Python Development/Python Advanced January 2020/Python Advanced/25. PAST EXAMS/07. C# Advanced Exam - 26 October 2019/01. Dating App.py
@@ -1,1 +1,41 @@
-# TODO Not Yet Solved of Uploaded
+from collections import deque
+
+males = [int(m) for m in input().split()]
+females = deque([int(f) for f in input().split()])
+
+matches = 0
+
+while males and females:
+    male = males[-1]
+    female = females[0]
+    if male % 25 == 0 or female % 25 == 0:
+        if male % 25 == 0:
+            males.pop()
+            # if males:
+            #     males.pop()
+        if female % 25 == 0:
+            females.popleft()
+            if females:
+                females.popleft()
+        continue
+    male = males.pop()
+    if male <= 0:
+        continue
+    female = females.popleft()
+    if female <= 0:
+        males.append(male)
+        continue
+    if male == female:
+        matches += 1
+    else:
+        males.append(male - 2)
+
+print(f"Matches: {matches}")
+if not males:
+    print(f"Males left: none")
+else:
+    print(f"Males left: {', '.join((str(male) for male in males[::-1]))}")
+if not females:
+    print(f"Females left: none")
+else:
+    print(f"Females left: {', '.join([str(female) for female in females])}")
--- SoftUni Lessons/Python Development/Python Advanced January 2020/Python Advanced/25. PAST EXAMS/07. C# Advanced Exam - 26 October 2019/02. Book Worm.py
+++ SoftUni Lessons/Python Development/Python Advanced January 2020/Python Advanced/25. PAST EXAMS/07. C# Advanced Exam - 26 October 2019/02. Book Worm.py
@@ -1,1 +1,40 @@
-# TODO Not Yet Solved of Uploaded
+text = input()
+
+matrix = []
+player = []
+
+moves = {  # This are the Delta changes for the moves:
+    "up": [-1, 0],
+    "down": [1, 0],
+    "left": [0, -1],
+    "right": [0, 1]
+}
+
+for row in range(int(input())):  # Build the matrix + get the position of the player
+    line = [x for x in input()]
+    matrix.append(line)
+    if "P" in line:
+        player = [row, line.index("P")]
+
+while True:
+    command = input()
+    if command == "end":
+        break
+    next_row, next_col = [player[0] + moves[command][0], player[1] + moves[command][1]]  # Get the future move coords.
+
+    if 0 <= next_row < len(matrix) and 0 <= next_col < len(matrix[0]):  # So the move is valid.
+        if matrix[next_row][next_col] != "-":  # If the cell is letter we will add it to the text
+            text += matrix[next_row][next_col]
+
+        matrix[player[0]][player[1]] = "-"  # Change the old players cell to empty "-"
+        matrix[next_row][next_col] = "P"  # Mark the new position of the player
+        player = [next_row, next_col]  # Change the players coords.
+
+    else:  # If the move is invalid we will remove the last letter of the text, if it still exists.
+        if text:
+            text = text[:-1]
+
+print(text)
+[print(''.join(row)) for row in matrix]  # Print the matrix
+
+
