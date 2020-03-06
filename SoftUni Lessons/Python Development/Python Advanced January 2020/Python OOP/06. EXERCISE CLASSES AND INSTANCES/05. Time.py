d04c0293e19e86a52ebb315b36f834421565e93e
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2020-03-06 16:02:07 +0200

    Update 05. Time.py

--- SoftUni Lessons/Python Development/Python Advanced January 2020/Python OOP/06. EXERCISE CLASSES AND INSTANCES/05. Time.py
+++ SoftUni Lessons/Python Development/Python Advanced January 2020/Python OOP/06. EXERCISE CLASSES AND INSTANCES/05. Time.py
@@ -14,38 +14,20 @@ class Time:
         self.hours, self.minutes, self.seconds = hours, minutes, seconds
 
     def get_time(self):
-        """returns the time formated"""
+        """returns the time formatted"""
         return f"{self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}"
 
-    # def next_second(self, secs_to_add=1):
-    #     secs = self.hours * 3600 + self.minutes * 60 + self.seconds
-    #     secs += secs_to_add
-    #
-    #     self.hours = secs // 3600
-    #     self.minutes = (secs % 3600) // 60
-    #     self.seconds = secs % 60
-    #
-    #     return self.get_time()
-
     def next_second(self):
-        """update the time with one second (use the class attributes for validation)
-         and return the new time (using the get_time() method).
-         If the hour gets greater than 24, set it to 0"""
-
-        if self.seconds + 1 > Time.max_seconds:
+        """Add one second to the time"""
+        self.seconds += 1
+        if self.seconds >= Time.max_seconds:
             self.seconds = 0
             self.minutes += 1
-            if self.minutes > Time.max_minutes:
+            if self.minutes >= Time.max_minutes:
                 self.minutes = 0
                 self.hours += 1
-                if self.hours == Time.max_hours:
+                if self.hours >= Time.max_hours:
                     self.hours = 0
-                else:
-                    self.hours += 1
-            else:
-                self.seconds += 1
-        else:
-            self.seconds += 1
         return self.get_time()
 
 
