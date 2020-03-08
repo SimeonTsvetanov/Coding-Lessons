bc8aeed2aec02e9c75e96c3ea5709982b226689f
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2020-03-09 00:39:32 +0200

    Upload the problems, but not yet solved
    the 2'nd task needs strong improvement as something is wrong with it. And it's too damn long to solve :D

---
+++ 02. Wild Farm.py
@@ -0,0 +1,151 @@
+# At the moment this solution gives 62 / 100 It will need future improvements...
+# And it seems that all the invalid tests are with invalid input.
+# So the logic it self is correct but I have messed up some where with the Abstractions ...
+from abc import ABC, abstractmethod
+
+
+class Food(ABC):
+    @abstractmethod
+    def __init__(self, quantity: int):
+        self.quantity = quantity
+
+
+class Vegetable(Food):
+    def __init__(self, quantity):
+        super().__init__(quantity)
+
+
+class Fruit(Food):
+    def __init__(self, quantity):
+        super().__init__(quantity)
+
+
+class Meat(Food):
+    def __init__(self, quantity):
+        super().__init__(quantity)
+
+
+class Seeds(Food):
+    def __init__(self, quantity):
+        super().__init__(quantity)
+
+
+class Animal(ABC):
+    @abstractmethod
+    def __init__(self, name: str, weight: float):
+        self.name = name
+        self.weight = weight
+        self.food_eaten = 0
+
+
+class Bird(Animal, ABC):
+    @abstractmethod
+    def __init__(self, name, weight, wing_size):
+        super().__init__(name, weight)
+        self.wing_size = wing_size
+
+    def __repr__(self):
+        return f"{self.__class__.__name__} [{self.name}, {self.wing_size}, {self.weight}, {self.food_eaten}]"
+
+
+class Owl(Bird):
+    def __init__(self, name, weight, wing_size):
+        super().__init__(name, weight, wing_size)
+
+    @staticmethod
+    def make_sound():
+        return "Hoot Hoot"
+
+    def feed(self, food):
+        if food.__class__.__name__ == "Meat":
+            self.weight += 0.25 * food.quantity
+            self.food_eaten += food.quantity
+        else:
+            return f"{self.__class__.__name__} does not eat {food.__class__.__name__}!"
+
+
+class Hen(Bird):
+    def __init__(self, name, weight, wing_size):
+        super().__init__(name, weight, wing_size)
+
+    @staticmethod
+    def make_sound():
+        return "Cluck"
+
+    def feed(self, food):
+        self.weight += 0.35 * food.quantity
+        self.food_eaten += food.quantity
+
+
+class Mammal(Animal, ABC):
+    @abstractmethod
+    def __init__(self, name, weight, living_region):
+        super().__init__(name, weight)
+        self.living_region = living_region
+
+    def __repr__(self):
+        return f"{self.__class__.__name__} [{self.name}, {self.weight}, {self.living_region}, {self.food_eaten}]"
+
+
+class Mouse(Mammal):
+    def __init__(self, name, weight, living_region):
+        super().__init__(name, weight, living_region)
+
+    @staticmethod
+    def make_sound():
+        return "Squeak"
+
+    def feed(self, food):
+        if food.__class__.__name__ in ["Vegetable", "Fruit"]:
+            self.weight += 0.10 * food.quantity
+            self.food_eaten += food.quantity
+        else:
+            return f"{self.__class__.__name__} does not eat {food.__class__.__name__}!"
+
+
+class Dog(Mammal):
+    def __init__(self, name, weight, living_region):
+        super().__init__(name, weight, living_region)
+
+    @staticmethod
+    def make_sound():
+        return "Woof!"
+
+    def feed(self, food):
+        if food.__class__.__name__ == "Meat":
+            self.weight += 0.40 * food.quantity
+            self.food_eaten += food.quantity
+        else:
+            return f"{self.__class__.__name__} does not eat {food.__class__.__name__}!"
+
+
+class Cat(Mammal):
+    def __init__(self, name, weight, living_region):
+        super().__init__(name, weight, living_region)
+
+    @staticmethod
+    def make_sound():
+        return "Meow"
+
+    def feed(self, food):
+        if food.__class__.__name__ in ["Vegetable", "Meat"]:
+            self.weight += 0.30 * food.quantity
+            self.food_eaten += food.quantity
+        else:
+            return f"{self.__class__.__name__} does not eat {food.__class__.__name__}!"
+
+
+class Tiger(Mammal):
+    def __init__(self, name, weight, living_region):
+        super().__init__(name, weight, living_region)
+
+    @staticmethod
+    def make_sound():
+        return "ROAR!!!"
+
+    def feed(self, food):
+        if food.__class__.__name__ == "Meat":
+            self.weight += 1.00 * food.quantity
+            self.food_eaten += food.quantity
+        else:
+            return f"{self.__class__.__name__} does not eat {food.__class__.__name__}!"
