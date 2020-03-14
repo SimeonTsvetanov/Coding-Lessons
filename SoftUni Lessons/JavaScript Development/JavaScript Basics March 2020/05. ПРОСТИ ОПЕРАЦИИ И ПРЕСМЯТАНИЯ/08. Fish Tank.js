59daca9c1cd0382344b9f1c8b6b095ee41c0fff3
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2020-03-14 15:59:16 +0200

    syntax fix

--- SoftUni Lessons/JavaScript Development/JavaScript Basics March 2020/05. ПРОСТИ ОПЕРАЦИИ И ПРЕСМЯТАНИЯ/08. Fish Tank.js
+++ SoftUni Lessons/JavaScript Development/JavaScript Basics March 2020/05. ПРОСТИ ОПЕРАЦИИ И ПРЕСМЯТАНИЯ/08. Fish Tank.js
@@ -1,13 +1,8 @@
 // Not included in final score
-function fishTank(input) {
-    let width = input.shift();
-    let length = input.shift();
-    let height = input.shift();
-    let procentage = input.shift();
-
+function fishTank(width, length, height, procentage) {
     let neededWater = (((width * length * height) / 1000) * (1 - (procentage * 0.01))).toFixed(3);
     console.log(neededWater);
 }
 
-fishTank(["85", "75", "47", "17"]);
-fishTank(["105", "77", "89", "18.5"]);
+fishTank(85, 75, 47, 17);
+fishTank(105, 77, 89, 18.5);
