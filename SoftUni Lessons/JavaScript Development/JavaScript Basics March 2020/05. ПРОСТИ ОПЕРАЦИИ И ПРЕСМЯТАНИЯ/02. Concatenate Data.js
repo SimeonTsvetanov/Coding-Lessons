9fc58f91212c3c3f72e98daa0299234979ad9671
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2020-03-14 15:56:03 +0200

    syntax fix

--- SoftUni Lessons/JavaScript Development/JavaScript Basics March 2020/05. ПРОСТИ ОПЕРАЦИИ И ПРЕСМЯТАНИЯ/02. Concatenate Data.js
+++ SoftUni Lessons/JavaScript Development/JavaScript Basics March 2020/05. ПРОСТИ ОПЕРАЦИИ И ПРЕСМЯТАНИЯ/02. Concatenate Data.js
@@ -1,11 +1,6 @@
-function concatenateData(input) {
-    let firstName = input.shift();
-    let lastName = input.shift();
-    let age = input.shift();
-    let town = input.shift();
-
+function concatenateData(firstName, lastName, age, town) {
     let message = `You are ${firstName} ${lastName}, a ${age}-years old person from ${town}. `;
     console.log(message);
 }
 
-concatenateData(["Maria","Ivanova",20,"Sofia"]);
+concatenateData("Maria","Ivanova",20,"Sofia");
