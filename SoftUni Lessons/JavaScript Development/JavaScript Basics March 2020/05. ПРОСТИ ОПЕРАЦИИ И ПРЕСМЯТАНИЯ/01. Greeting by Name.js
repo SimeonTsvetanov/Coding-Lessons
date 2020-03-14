0eb4b7c2dce524f5e419d332feca4d9dfd653876
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2020-03-14 15:55:38 +0200

    fixing with the new syntax

--- SoftUni Lessons/JavaScript Development/JavaScript Basics March 2020/05. ПРОСТИ ОПЕРАЦИИ И ПРЕСМЯТАНИЯ/01. Greeting by Name.js
+++ SoftUni Lessons/JavaScript Development/JavaScript Basics March 2020/05. ПРОСТИ ОПЕРАЦИИ И ПРЕСМЯТАНИЯ/01. Greeting by Name.js
@@ -1,10 +1,6 @@
-function greeting(input) {
-    let name = input.shift();
-    // let sayHello = "Hello, " + name + "!";
+function greeting(name) {
     console.log(`Hello, ${name}!`);
-
-    // Same code but in Python: 
-    //print(f"Hello, {input()}!")
 }
 
-greeting(["Simeon Tsvetanov"]);
+greeting("Simeon Tsvetanov");
+
