986cdc553c17bf2420ed492f4c4461cceb2f7be1
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2020-03-21 22:53:35 +0200

    Update 03. Even or Odd.js

--- SoftUni Lessons/JavaScript Development/JavaScript Basics March 2020/07. ПРОВЕРКИ/03. Even or Odd.js
+++ SoftUni Lessons/JavaScript Development/JavaScript Basics March 2020/07. ПРОВЕРКИ/03. Even or Odd.js
@@ -1,5 +1,5 @@
-// There are two ways of reading the Input for this tasks:
-// This is the new way:
+// Short link: https://bit.ly/2Wu3orU
+
 function evenOrOdd(number) {
     if (number % 2 === 0) {
         console.log("even")
@@ -12,20 +12,3 @@ evenOrOdd(2); // Expected output: even
 evenOrOdd(3); // Expected output: odd
 evenOrOdd(25); // Expected output: odd
 evenOrOdd(1024); // Expected output: even
-
-
-// -----------------------------------------------------------------------//
-// This is the old way of reading the input:
-function evenOrOddOld(input) {
-    number = Number(input.shift());
-    if (number % 2 === 0) {
-        console.log("even")
-    } else {
-        console.log("odd")
-    }
-}
-
-evenOrOddOld([2]); // Expected output: even
-evenOrOddOld([3]); // Expected output: odd
-evenOrOddOld([25]); // Expected output: odd
-evenOrOddOld([1024]); // Expected output: even
