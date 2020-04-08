375b3b50621b1d04fa684c8e650d8a31f01cd7fb
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2020-04-08 23:22:48 +0300

    upload the 4th an the 5th problems solved.

--- SoftUni Lessons/JavaScript Development/JavaScript Basics March 2020/21. МИНАЛИ ИЗПИТИ/02.  Online Retake Exam - 2 and 3 May 2019/04. Tourist Shop.js
+++ SoftUni Lessons/JavaScript Development/JavaScript Basics March 2020/21. МИНАЛИ ИЗПИТИ/02.  Online Retake Exam - 2 and 3 May 2019/04. Tourist Shop.js
@@ -1,1 +1,35 @@
-// Not Done Yet!
+function touristShop(input) {
+    // Mask
+    let budget = Number(input.shift());
+    let products = 0;
+    let total = 0;
+
+    while('I am an idiot!') {
+        let product = input.shift(); 
+        if (product == 'Stop') {
+            console.log(`You bought ${products} products for ${total.toFixed(2)} leva.`);
+            break;
+        }
+        products += 1;
+        let price = Number(input.shift());
+        if (products % 3 == 0) {
+            price = price / 2;
+        }
+
+        if (budget - price < 0) {
+            console.log(`You don't have enough money!`);
+            console.log(`You need ${(price - budget).toFixed(2)} leva!`);
+            break;
+        }
+        
+        total += price;
+        budget -= price;
+    }
+}
+
+touristShop([153.20, 'Backpack', 25.20, 'Shoes', 54, 'Sunglasses', 30, 'Stop']);  // Should return:
+// You bought 3 products for 94.20 leva.
+
+touristShop([54, 'Thermal underwear', 24, 'Sunscreen', 45]);  // Should return:
+// You don't have enough money!
+// You need 15.00 leva!
--- SoftUni Lessons/JavaScript Development/JavaScript Basics March 2020/21. МИНАЛИ ИЗПИТИ/02.  Online Retake Exam - 2 and 3 May 2019/05. Division Without Remainder.js
+++ SoftUni Lessons/JavaScript Development/JavaScript Basics March 2020/21. МИНАЛИ ИЗПИТИ/02.  Online Retake Exam - 2 and 3 May 2019/05. Division Without Remainder.js
@@ -1,1 +1,35 @@
-// Not Done Yet!
+function devisionWithoutreminder(input) {
+    // Mask
+    let total = Number(input.shift());
+
+    let two = 0;
+    let three = 0;
+    let four = 0;
+
+    for (i = 1; i <= total; i += 1) {
+        num = Number(input.shift());
+        if (num % 2 == 0) {
+            two += 1;
+        }
+        if (num % 3 == 0) {
+            three += 1;
+        }
+        if (num % 4 == 0) {
+            four += 1;
+        }
+    }
+
+    console.log(`${(two / total * 100).toFixed(2)}%`);
+    console.log(`${(three / total * 100).toFixed(2)}%`);
+    console.log(`${(four / total * 100).toFixed(2)}%`);
+}
+
+devisionWithoutreminder([10, 680, 2, 600, 200, 800, 799, 199, 46, 128, 65,]);  // Should return:
+// 70.00%
+// 10.00%
+// 50.00%
+
+devisionWithoutreminder([3, 3, 6, 9]);  // Should return:
+// 33.33%
+// 100.00%
+// 0.00%
