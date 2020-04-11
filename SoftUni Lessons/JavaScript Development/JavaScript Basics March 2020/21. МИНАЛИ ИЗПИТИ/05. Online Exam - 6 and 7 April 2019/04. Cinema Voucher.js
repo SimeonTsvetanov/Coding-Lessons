02de3ae987053e583366861371a3aee9f92f5db9
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2020-04-11 11:59:15 +0300

    add 4 and 5

--- SoftUni Lessons/JavaScript Development/JavaScript Basics March 2020/21. МИНАЛИ ИЗПИТИ/05. Online Exam - 6 and 7 April 2019/04. Cinema Voucher.js
+++ SoftUni Lessons/JavaScript Development/JavaScript Basics March 2020/21. МИНАЛИ ИЗПИТИ/05. Online Exam - 6 and 7 April 2019/04. Cinema Voucher.js
@@ -1,1 +1,39 @@
-// Under Construction
+function cinemaVouchers(input) {
+    // Mask
+    let vaucher = Number(input.shift());
+
+    let tickets = 0;
+    let products = 0;
+
+    while ('End, or No more VCH') {
+        let name = input.shift();
+        if (name == "End") {
+            break;
+        }
+
+        if (name.length > 8) {
+            // It's a Movie (SIT DOWN AND GET DUM)
+            let price = name.charCodeAt(0) + name.charCodeAt(1);
+            if (vaucher - price < 0) {break;}
+            vaucher -= price
+            tickets += 1
+        } else {
+            // IT's a Product: (OVERPRICED FAT MAKER)
+            let price = name.charCodeAt(0);
+            if (vaucher - price < 0) {break;}
+            vaucher -= price
+            products += 1
+        }
+    }
+    
+    console.log(tickets);
+    console.log(products);
+}
+
+cinemaVouchers([300, 'Captain Marvel', 'popcorn', 'Pepsi']);  // Should return:
+// 1
+// 1
+
+cinemaVouchers([1500, 'Avengers: Endgame', 'Bohemian Rhapsody', 'Deadpool 2', 'End']);  // Should return:
+// 3
+// 0
--- SoftUni Lessons/JavaScript Development/JavaScript Basics March 2020/21. МИНАЛИ ИЗПИТИ/05. Online Exam - 6 and 7 April 2019/05. Movie Ratings.js
+++ SoftUni Lessons/JavaScript Development/JavaScript Basics March 2020/21. МИНАЛИ ИЗПИТИ/05. Online Exam - 6 and 7 April 2019/05. Movie Ratings.js
@@ -1,1 +1,43 @@
-// Under Construction
+function movieRatings(input) {
+    // Mask
+    let count = Number(input.shift());
+    
+    let bestRatedName = '';
+    let bestRating = Number.MIN_SAFE_INTEGER;
+    let worstRatedName = '';
+    let worstRating = Number.MAX_SAFE_INTEGER;
+    let totalScores = 0;
+
+    for(let movie = 1; movie <= count; movie += 1) {
+        let name = input.shift();
+        let rating = Number(input.shift());
+
+        if (rating > bestRating) {
+            bestRatedName = name;
+            bestRating = rating;
+        }
+
+        if (rating < worstRating) {
+            worstRatedName = name;
+            worstRating = rating;
+        }
+    
+        totalScores += rating;
+    }
+
+    console.log(`${bestRatedName} is with highest rating: ${bestRating.toFixed(1)}`);
+    console.log(`${worstRatedName} is with lowest rating: ${worstRating.toFixed(1)}`);
+    console.log(`Average rating: ${(totalScores / count).toFixed(1)}`);
+}
+
+movieRatings([5, 'A Star is Born', 7.8, 'Creed 2', 7.3, 'Mary Poppins', 7.2, 'Vice', 7.2, 'Captain Marvel', 7.1]);  
+// Should return:
+// A Star is Born is with highest rating: 7.8
+// Captain Marvel is with lowest rating: 7.1
+// Average rating: 7.3
+
+movieRatings([3, 'Interstellar', 8.5, 'Dangal', 8.3, 'Green Book', 8.2]);  
+// Should return:
+// Interstellar is with highest rating: 8.5
+// Green Book is with lowest rating: 8.2
+// Average rating: 8.3
