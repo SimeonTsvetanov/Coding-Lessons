66665f13a9901bdadae784d4e417ac6316d45a30
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2020-04-13 21:40:38 +0300

    Upload the 6th tasks

--- SoftUni Lessons/JavaScript Development/JavaScript Basics March 2020/21. МИНАЛИ ИЗПИТИ/01. Online Exam - 20 and 21 April 2019/06. Easter Competition.js
+++ SoftUni Lessons/JavaScript Development/JavaScript Basics March 2020/21. МИНАЛИ ИЗПИТИ/01. Online Exam - 20 and 21 April 2019/06. Easter Competition.js
@@ -1,1 +1,45 @@
-// Not yet Done!
+function easterCompetition(input) {
+    // Mask
+    let count = Number(input.shift());
+
+    let bestChef = undefined;
+    let bestPoints = Number.MIN_SAFE_INTEGER;
+
+    for (let times = 1; times <= count; times += 1) {
+        let chef = input.shift();
+        let points = 0;
+        while ('command is Stop') {
+            let command = input.shift();
+            if (command == "Stop") {
+                console.log(`${chef} has ${points} points.`);
+                if (points > bestPoints) {
+                    bestChef = chef;
+                    bestPoints = points;
+                    console.log(`${chef} is the new number 1!`);
+                }
+                break;
+            }
+            points += Number(command);
+
+        }
+    }
+
+    console.log(`${bestChef} won competition with ${bestPoints} points!`);
+}
+
+easterCompetition([3, 'Chef Manchev', 10, 10, 10, 10, 'Stop', 'Natalie', 8, 2, 9, 'Stop', 'George', 9, 2, 4, 2, 'Stop']); 
+// Should return:
+// Chef Manchev has 40 points.
+// Chef Manchev is the new number 1!
+// Natalie has 19 points.
+// George has 17 points.
+// Chef Manchev won competition with 40 points!
+
+
+easterCompetition([2, 'Chef Angelov', 9, 9, 9, 'Stop', 'Chef Rowe', 10, 10, 10, 10, 'Stop']);  
+// Should return:
+// Chef Angelov has 27 points.
+// Chef Angelov is the new number 1!
+// Chef Rowe has 40 points.
+// Chef Rowe is the new number 1!
+// Chef Rowe won competition with 40 points!
--- SoftUni Lessons/JavaScript Development/JavaScript Basics March 2020/21. МИНАЛИ ИЗПИТИ/01. Online Exam - 20 and 21 April 2019/06. Easter Decoration.js
+++ SoftUni Lessons/JavaScript Development/JavaScript Basics March 2020/21. МИНАЛИ ИЗПИТИ/01. Online Exam - 20 and 21 April 2019/06. Easter Decoration.js
@@ -1,1 +1,38 @@
-// Not yet Done!
+function easterDecoration(input) {
+    // Mask
+    let clients = Number(input.shift());
+    
+    let prices = {'basket': 1.50, 'wreath': 3.80, 'chocolate bunny': 7};
+    let total = 0;
+    
+    for (let client = 1; client <= clients; client += 1) {
+        let count = 0;
+        let price = 0;
+        while('commmand is Finish') {
+            let product = input.shift();
+            if (product == "Finish") {
+                // TODO
+                if (count % 2 == 0) {price *= 0.8;}
+                total += price;
+                console.log(`You purchased ${count} items for ${price.toFixed(2)} leva.`);
+                break;
+            }
+            price += prices[product];
+            count += 1;
+        }
+    }
+
+    console.log(`Average bill per client is: ${(total / clients).toFixed(2)} leva.`);
+}
+
+easterDecoration([2, 'basket', 'wreath', 'chocolate bunny', 'Finish', 'wreath', 'chocolate bunny', 'Finish']);  
+// Should return:
+// You purchased 3 items for 12.30 leva.
+// You purchased 2 items for 8.64 leva.
+// Average bill per client is: 10.47 leva.
+
+
+easterDecoration([1, 'basket', 'wreath', 'chocolate bunny', 'wreath', 'basket', 'chocolate bunny', 'Finish']);  
+// Should return:
+// You purchased 6 items for 19.68 leva.
+// Average bill per client is: 19.68 leva.
