5867c6c46f0eca5494b6ccaafe97132531271e72
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2020-04-13 23:36:13 +0300

    Upload the 6th tasks solved

--- SoftUni Lessons/JavaScript Development/JavaScript Basics March 2020/21. МИНАЛИ ИЗПИТИ/03. Online Exam - 9 and 10 March 2019/06. Basketball Tournament.js
+++ SoftUni Lessons/JavaScript Development/JavaScript Basics March 2020/21. МИНАЛИ ИЗПИТИ/03. Online Exam - 9 and 10 March 2019/06. Basketball Tournament.js
@@ -1,1 +1,56 @@
-// Under Construction
+function basketballTournament(input) {
+    // Mask
+    let tournamentName = undefined;
+    let countGames = 0;
+    let pointsOne = 0;
+    let pointsTwo = 0;
+    let gamesWon = 0;
+    let gamesLost = 0;
+
+    while ('I say go!') {
+        tournamentName = input.shift();
+        if (tournamentName == "End of tournaments") {break;}
+        countGames = Number(input.shift());
+        for (game = 1; game <= countGames; game +=1) {
+            let pointsOne = Number(input.shift());
+            let pointsTwo = Number(input.shift());
+            if (pointsOne > pointsTwo) {
+                console.log(`Game ${game} of tournament ${tournamentName}: win with ${pointsOne - pointsTwo} points.`);
+                gamesWon += 1;
+            } else {
+                console.log(`Game ${game} of tournament ${tournamentName}: lost with ${pointsTwo - pointsOne} points.`);
+                gamesLost += 1;
+            }
+        }
+        
+    }
+
+    let totalGames = gamesWon + gamesLost;
+
+    console.log(`${(gamesWon / totalGames * 100).toFixed(2)}% matches win`);
+    console.log(`${(gamesLost / totalGames * 100).toFixed(2)}% matches lost`);
+}
+
+
+basketballTournament(['Dunkers', 2, 75, 65, 56, 73, 'Fire Girls', 3, 67, 34, 83, 98, 66, 45, 'End of tournaments']);  
+// Should return:
+// Game 1 of tournament Dunkers: win with 10 points.
+// Game 2 of tournament Dunkers: lost with 17 points.
+// Game 1 of tournament Fire Girls: win with 33 points.
+// Game 2 of tournament Fire Girls: lost with 15 points.
+// Game 3 of tournament Fire Girls: win with 21 points.
+// 60.00% matches win
+// 40.00% matches lost
+
+
+basketballTournament(['Ballers', 3, 87, 63, 56, 65, 75, 64, 'Sharks', 4, 64, 76, 65, 86, 68, 99, 45, 78, 'End of tournaments ']);  
+// Should return:
+// Game 1 of tournament Ballers: win with 24 points.
+// Game 2 of tournament Ballers: lost with 9 points.
+// Game 3 of tournament Ballers: win with 11 points.
+// Game 1 of tournament Sharks: lost with 12 points.
+// Game 2 of tournament Sharks: lost with 21 points.
+// Game 3 of tournament Sharks: lost with 31 points.
+// Game 4 of tournament Sharks: lost with 33 points.
+// 28.57% matches win
+// 71.43% matches lost
--- SoftUni Lessons/JavaScript Development/JavaScript Basics March 2020/21. МИНАЛИ ИЗПИТИ/03. Online Exam - 9 and 10 March 2019/06. High Jump.js
+++ SoftUni Lessons/JavaScript Development/JavaScript Basics March 2020/21. МИНАЛИ ИЗПИТИ/03. Online Exam - 9 and 10 March 2019/06. High Jump.js
@@ -1,1 +1,31 @@
-// Under Construction
+function highJump(input) {
+    // Mask
+    let aim = Number(input.shift());
+    let start = aim - 30;
+    let badCount = 0;
+    let totalJumps = 0;
+    
+    while (!(start > aim)) {
+        if (badCount == 3) {break;}
+        let jump = Number(input.shift());
+        totalJumps += 1;
+        if (jump > start) {
+            start += 5;
+            badCount = 0;
+        } else {
+            badCount += 1;
+        }
+    }
+
+    if (badCount == 3) {
+        console.log(`Tihomir failed at ${start}cm after ${totalJumps} jumps.`);
+    } else {
+        console.log(`Tihomir succeeded, he jumped over ${start - 5}cm after ${totalJumps} jumps.`)
+    }
+}
+
+highJump([231, 205, 212, 213, 228, 229, 230, 235]);  // Should return:
+// Tihomir succeeded, he jumped over 231cm after 7 jumps.
+
+highJump([250, 225, 224, 225, 228, 231, 235, 234, 235]);  // Should return:
+// Tihomir failed at 235cm after 8 jumps.
