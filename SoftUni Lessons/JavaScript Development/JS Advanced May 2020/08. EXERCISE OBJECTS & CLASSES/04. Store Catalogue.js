4095fc9e34697e17b4b2fc36f5de52154e4c06d3
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2020-06-01 21:57:06 +0300

    add comments.

--- SoftUni Lessons/JavaScript Development/JS Advanced May 2020/08. EXERCISE OBJECTS & CLASSES/04. Store Catalogue.js
+++ SoftUni Lessons/JavaScript Development/JS Advanced May 2020/08. EXERCISE OBJECTS & CLASSES/04. Store Catalogue.js
@@ -3,14 +3,21 @@ function storeCatalogue(input) {
     let catalogue = {};
     input.map(data => { catalogue[data.split(' : ')[0]] = Number(data.split(' : ')[1]); });
 
-    let letter = ''
+    let letter = '' // Here We will keep the current letter to Print... 
+    
     // The sort method doesn't require a second argument for sort by value BUT it it's for testing :).
-    Array.from(Object.keys(catalogue)).sort((a, b) => { return a.localeCompare(b) || catalogue[a] - catalogue[b] }).map(product => {
+    // First, we will create Array from the KEYS of the Catalogue.
+    Array.from(Object.keys(catalogue))
+        // Then we will sort the ARRAY First Alphabetically and then if the names are the same: by value
+        .sort((a, b) => { return a.localeCompare(b) || catalogue[a] - catalogue[b] })
+        // And the we iterate the SORTED ARRAY element by element to print it.
+        .map(product => {
+        // But first we will check if the letter need's to be printed: 
         if (product[0] !== letter) {
             letter = product[0];
             console.log(product[0]);
         }
-        console.log(`  ${product}: ${catalogue[product]}`);
+        console.log(`  ${product}: ${catalogue[product]}`);  // And Finally we will print the output line.
     });
 }
 
