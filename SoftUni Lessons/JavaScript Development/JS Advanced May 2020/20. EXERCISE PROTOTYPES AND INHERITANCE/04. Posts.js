3ec18b63bc892fb38b82c12056015e5d5d30c451
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2020-06-21 04:47:42 +0300

    Upload Solved!

---
+++ SoftUni Lessons/JavaScript Development/JS Advanced May 2020/20. EXERCISE PROTOTYPES AND INHERITANCE/04. Posts.js
@@ -0,0 +1,58 @@
+function posts() {
+    class Post {
+        constructor(title, content) {
+            this.title = title;
+            this.content = content;
+        }
+        toString() {
+            return `Post: ${this.title}\nContent: ${this.content}`
+        }
+    }
+
+    class SocialMediaPost extends Post{
+        constructor(title, content, likes, dislikes) {
+            super(title, content);
+            this.likes = likes;
+            this.dislikes = dislikes;
+            this.comments = [];
+        }
+
+        addComment(comment) {
+            this.comments.push(comment);
+        }
+
+        toString() {
+            let result = `Post: ${this.title}\nContent: ${this.content}\nRating: ${this.likes - this.dislikes}`;
+            if (this.comments.length > 0) {
+                result += `\nComments:`;
+                this.comments.map(comment => result += `\n * ${comment}`);
+            }
+            return result;
+        }
+
+    }
+
+    class BlogPost extends Post {
+        constructor(title, content, views) {
+            super(title, content);
+            this.views = views;
+        }
+
+        view() {
+            this.views += 1;
+            return this;
+        }
+
+        toString() {
+            return super.toString() + `\nViews: ${this.views}`;
+        }
+    }
+
+    return {
+        Post,
+        SocialMediaPost,
+        BlogPost,
+    }
+}
+
+posts();
