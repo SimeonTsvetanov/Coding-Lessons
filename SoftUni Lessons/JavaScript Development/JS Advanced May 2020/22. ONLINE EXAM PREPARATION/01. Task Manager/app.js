0e778c11dc1a6a8022714453cc7c86cb974b45c5
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2020-06-26 22:09:16 +0300

    Add the Short Version

--- SoftUni Lessons/JavaScript Development/JS Advanced May 2020/22. ONLINE EXAM PREPARATION/01. Task Manager/app.js
+++ SoftUni Lessons/JavaScript Development/JS Advanced May 2020/22. ONLINE EXAM PREPARATION/01. Task Manager/app.js
@@ -1,103 +1,128 @@
+// First is the short version and second is the same task but easy on the eyes:
 function solve() {
-    // Get the sections:
-    let addTaskSection = document.querySelectorAll('section')[0].lastElementChild // Add Task
-    let openTaskSection = document.querySelectorAll('section')[1].lastElementChild // Open Task
-    let inProgressTaskSection = document.querySelectorAll('section')[2].lastElementChild // In Progress
-    let completeTaskSection = document.querySelectorAll('section')[3].lastElementChild // Complete
-
-    // Get the Task Input
-    let taskNameField = document.querySelector('#task');
-    let taskDecrField = document.querySelector('#description');
-    let taskDateField = document.querySelector('#date');
-
-    // Get the Add button:
-    let addButton = document.querySelector("#add");
-
-    // Create the Element when add is clicked:
-    addButton.addEventListener('click', addTask);
-
-    function addTask(e) {
-        // Stop if from Reloading:
+    let openMenu = document.querySelectorAll('section').item(1).lastElementChild;
+    let inProgressMenu = document.querySelectorAll('section').item(2).lastElementChild;
+    let completeMenu = document.querySelectorAll('section').item(3).lastElementChild;
+    document.querySelector('#add').addEventListener('click', mask);
+    function mask(e) {
         e.preventDefault();
-
-        // Validate the input:
-        if (!(taskNameField.value && taskDecrField.value && taskDateField.value)) { return; }
-
-        // Create the Element
-        let article = l('article', [
-            l('h3', taskNameField.value),
-            l('p', `Description: ${taskDecrField.value}`),
-            l('p', `Due Date: ${taskDateField.value}`),
-            l('div', [
-                l('button', 'Start', {class: 'green'}),
-                l('button', 'Delete', {class: 'red'}),
-            ], {class: 'flex'})
-        ]);
-
-        // Add the Event Listener on the Task:
-        article.addEventListener('click', workOnArticle);
-
-        // Append the Article to the Open Section:
-        openTaskSection.appendChild(article);
-
-        // Clear the input fields:
-        taskNameField.value = ''; taskDecrField.value = ''; taskDateField.value = '';
-    }
-
-    function workOnArticle(e) {
-        if (e.target.className === 'green') {
-            // Clicked on start
-            startTask(e);
-        } else if (e.target.className === 'red') {
-            // Clicked on Delete
-            deleteTask(e);
-        } else if (e.target.className === 'orange') {
-            // Clicked on Finish
-            finishTask(e);
-        }
-    }
-
-    function startTask(e) {
-        let task = e.target.parentElement.parentElement;
-
-        // Change the Section
-        inProgressTaskSection.appendChild(task);
-        // Remove the Start Button
-        let buttons = e.target.parentElement;
-        let startButton = e.target.parentElement.firstElementChild;
-        buttons.removeChild(startButton);
-
-        // Add the Finish Button
-        let finishButton = l('button', 'Finish', {class: 'orange'});
-        buttons.appendChild(finishButton);
-    }
-
-    function deleteTask(e) {
-        let section = e.target.parentElement.parentElement.parentElement;
-        let task = e.target.parentElement.parentElement;
-        section.removeChild(task);
-    }
-
-    function finishTask(e) {
-        let task = e.target.parentElement.parentElement;
-        completeTaskSection.appendChild(task);
-        let buttons = e.target.parentElement;
-        task.removeChild(buttons);
-    }
-
-    function l(type, content, ...attr) {
-        // 'Type', 'content' / ['content', l(another)], - [{className: 'className', {id: 'idName'}]
-        const r = document.createElement(type);
-        attr !== undefined ? attr.forEach(x => {
-            r.setAttribute(Object.keys(x)[0], Object.values(x)[0]);
-        }) : 'pass';
-        Array.isArray(content) ? content.forEach(append) : append(content);
-        function append(node) {
-            typeof node === 'string' ? node = document.createTextNode(node) : 'do nothing';
-            r.appendChild(node);
-        }
-        return r
+        let titleField = document.querySelector("#task");
+        let descField = document.querySelector('#description');
+        let dateField = document.querySelector('#date');
+        let title = titleField.value; let desc = descField.value; let date = dateField.value;
+        if (!(title && desc && date)) { return; }
+        let task = document.createElement('article');
+        task.innerHTML = `<h3>${title}</h3><p>Description: ${desc}</p><p>Due Date: ${date}</p><div class="flex"><button class="green">Start</button><button class="red">Delete</button></div>`;
+        task.addEventListener('click', (e) => {
+            if (e.target.textContent === 'Start') {
+                task.querySelector('.flex').removeChild(task.querySelector('.green'));
+                let finishBtn = document.createElement('button');
+                finishBtn.classList.add('orange');
+                finishBtn.textContent = 'Finish';
+                task.querySelector('.flex').appendChild(finishBtn);
+                inProgressMenu.appendChild(task);
+            } else if (e.target.textContent === 'Delete') {
+                task.parentElement.removeChild(task);
+            } else if (e.target.textContent === 'Finish') {
+                task.removeChild(task.lastElementChild);
+                completeMenu.appendChild(task);
+            }
+        });
+        openMenu.appendChild(task);
+        titleField.value = ''; descField.value = ''; dateField.value = '';
     }
 }
 
 
+function solveLongerButBetterVersion() {
+    // Get the Add button;
+    let addButton = document.getElementById('add');
+
+    // Add the event listener to the Add Button:
+    addButton.addEventListener("click", add);
+
+    function add(e) {
+        e.preventDefault();  // needed for easy debugging
+        let addMenu = e.target.parentElement;  // Get the Parent Element;
+        let task = document.querySelector('#task');  // Task Input Area
+        let description = document.querySelector('#description');  // Dec. input Area
+        let date = document.querySelector('#date');  // Date Input Area
+        // If any of the input fields is empty, the function doesn’t make anything
+        if (!(task.value && description.value && date.value)) {
+            return;
+        }
+
+        /*
+        We can do the next part using Inner HTML But the eventListeners will be hard to apply:
+        newTask.innerHTML = `<h3>${task.value}</h3><p>Description: ${description.value}</p><p>Due Date: ${date.value}</p><div class="flex"><button class="green">Start</button><button class="red">Delete</button></div>`;
+        If we add it in ths way we will need to set event listeners in a diff. way.
+        */
+
+        // Lets Create the task:
+        let article = document.createElement('article');
+        // Create All the Features for the article.
+        // Title
+        let h3Task = document.createElement('h3');
+        h3Task.textContent = task.value;
+        // Description
+        let pDescription = document.createElement('p');
+        pDescription.textContent = `Description: ${description.value}`;
+        // Date
+        let pDate = document.createElement('p');
+        pDate.textContent = `Due Date: ${date.value}`;
+        // Div for the buttons
+        let divButtons = document.createElement('div');
+        divButtons.classList.add('flex');
+        // Start Button
+        let startButton = document.createElement("button");
+        startButton.classList.add('green');
+        startButton.textContent = 'Start';
+        startButton.addEventListener('click', start);
+        // Delete Button
+        let deleteButton = document.createElement('button');
+        deleteButton.classList.add('red');
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', del);
+
+        // Add All the features to the Article:
+        divButtons.appendChild(startButton);
+        divButtons.appendChild(deleteButton);
+        article.appendChild(h3Task);
+        article.appendChild(pDescription);
+        article.appendChild(pDate);
+        article.appendChild(divButtons);
+
+        // Get the Open tasks menu:
+        let openTasksMenu = document.querySelectorAll("section")[1].children[1];
+        // And Append it to the OpenTaskMenuMenu:
+        openTasksMenu.appendChild(article);
+        // Clear the Add menu Data:
+        task.value = ''; description.value = ''; date.value = '';
+    }
+
+    function start(e) {
+        let inProgressMenu = document.querySelectorAll("section")[2].children[1]; // Get the whole menu
+        let wholeArticle = e.target.parentNode.parentNode;  // Get the selected Article
+        wholeArticle.querySelector("button").remove();  // Remove the Start Button
+
+        let finishButton = document.createElement("button");  // Crete the finish button
+        finishButton.classList.add("orange");  // Add the class orange to it
+        finishButton.textContent = "Finish";  // Add the text Finish
+        finishButton.addEventListener("click", finish);  // set the onclick function
+        wholeArticle.querySelector(".flex").appendChild(finishButton); // add the button to the article
+        inProgressMenu.appendChild(wholeArticle);  // ad the article to the menu
+    }
+
+    function del(e) {
+        let wholeArticle = e.target.parentNode.parentNode;  // Get the selected article
+        wholeArticle.remove(); // And Remove it.
+    }
+
+    function finish(e) {
+        let wholeArticle = e.target.parentNode.parentNode; // Get the selected Article
+        let completeMenu = document.querySelectorAll("section")[3].children[1];  // Get the menu
+        wholeArticle.querySelector("button").parentElement.remove();  // Remove the buttons
+        wholeArticle.remove();  // Remove the article from the current position
+        completeMenu.appendChild(wholeArticle); // Place it in the Finish area
+    }
+}
