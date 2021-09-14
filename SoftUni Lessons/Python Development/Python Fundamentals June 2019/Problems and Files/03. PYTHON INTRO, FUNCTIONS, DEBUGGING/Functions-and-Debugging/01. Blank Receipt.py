4c9d446978f43da6a45d93cef59943a4620da4c3
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2021-09-14 12:52:35 +0300

    Update 01. Blank Receipt.py
    adding the redo solution

--- SoftUni Lessons/Python Development/Python Fundamentals June 2019/Problems and Files/03. PYTHON INTRO, FUNCTIONS, DEBUGGING/Functions-and-Debugging/01. Blank Receipt.py
+++ SoftUni Lessons/Python Development/Python Fundamentals June 2019/Problems and Files/03. PYTHON INTRO, FUNCTIONS, DEBUGGING/Functions-and-Debugging/01. Blank Receipt.py
@@ -51,3 +51,21 @@ def print_all():
 
 
 print_all()
+
+def redo():
+    # Redoing the rask 
+    def print_header():
+        print("CASH RECEIPT\n------------------------------")
+
+    def print_body():
+        print("Charged to____________________\nReceived by___________________")
+
+    def print_footer():
+        print("------------------------------\n\u00A9 SoftUni")
+
+    def print_receipt():
+        print_header()
+        print_body()
+        print_footer()
+
+    print_receipt()
