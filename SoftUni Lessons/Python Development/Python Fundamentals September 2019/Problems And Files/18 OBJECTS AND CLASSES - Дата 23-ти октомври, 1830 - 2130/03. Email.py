19fb475dc841b9018a447da35cd484380edde1c4
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2021-09-22 13:16:10 +0300

    Update 03. Email.py
    add another solution

--- SoftUni Lessons/Python Development/Python Fundamentals September 2019/Problems And Files/18 OBJECTS AND CLASSES - Дата 23-ти октомври, 1830 - 2130/03. Email.py
+++ SoftUni Lessons/Python Development/Python Fundamentals September 2019/Problems And Files/18 OBJECTS AND CLASSES - Дата 23-ти октомври, 1830 - 2130/03. Email.py
@@ -57,3 +57,29 @@ for email in list(map(int, input().split(", "))):
 
 [print(email.get_info()) for email in all_emails]
 
+
+def email_another_solution():
+    class Email:
+        def __init__(self, sender: str, receiver: str, content: str):
+            self.sender = sender
+            self.receiver = receiver
+            self.content = content
+            self.is_sent = False
+
+        def send(self):
+            self.is_sent = True
+
+        def get_info(self):
+            return f"{self.sender} says to {self.receiver}: {self.content}. Sent: {self.is_sent}"
+
+    emails = []
+
+    while True:
+        command = input().split()
+        if command[0] == "Stop":
+            break
+        emails.append(Email(sender=command[0], receiver=command[1], content=command[2]))
+
+    [emails[email].send() for email in list(map(int, input().split(", ")))]
+    [print(email.get_info()) for email in emails]
+
