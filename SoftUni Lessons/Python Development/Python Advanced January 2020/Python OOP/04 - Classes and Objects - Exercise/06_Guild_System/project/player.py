99c6bf9cca6c9369d2ece83f06d8c5f0fd6c6295
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2022-04-01 11:26:19 +0300

    Add files via upload

---
+++ SoftUni Lessons/Python Development/Python Advanced January 2020/Python OOP/04 - Classes and Objects - Exercise/06_Guild_System/project/player.py
@@ -0,0 +1,24 @@
+class Player:
+    def __init__(self, name: str, hp: int, mp: int):
+        self.name = name
+        self.hp = hp
+        self.mp = mp
+        self.skills = {}
+        self.guild = "Unaffiliated"
+
+    def add_skill(self, skill_name, mana_cost):
+        if skill_name in self.skills:
+            return f"Skill already added"
+        self.skills[skill_name] = mana_cost
+        return f"Skill {skill_name} added to the collection of the player {self.name}"
+
+    def player_info(self):
+        result = f"Name: {self.name}\nGuild: {self.guild}\nHP: {self.hp}\nMP: {self.mp}\n"
+        for s, m in self.skills.items():
+            result += f"==={s} - {m}\n"
+        return result
+
+
+# player = Player("George", 50, 100)
+# print(player.add_skill("Shield Break", 20))
+# print(player.player_info())
