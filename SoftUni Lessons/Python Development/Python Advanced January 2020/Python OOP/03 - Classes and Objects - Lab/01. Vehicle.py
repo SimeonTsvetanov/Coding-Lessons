734241c59befd3af1227f3a747d4da61d1254504
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2022-04-01 11:33:02 +0300

    Add files via upload

---
+++ SoftUni Lessons/Python Development/Python Advanced January 2020/Python OOP/03 - Classes and Objects - Lab/01. Vehicle.py
@@ -0,0 +1,13 @@
+class Vehicle:
+    def __init__(self, mileage, max_speed=150):
+        self.max_speed = max_speed
+        self.mileage = mileage
+        self.gadgets = []
+
+
+car = Vehicle(20)
+print(car.max_speed)
+print(car.mileage)
+print(car.gadgets)
+car.gadgets.append('Hudly Wireless')
+print(car.gadgets)
