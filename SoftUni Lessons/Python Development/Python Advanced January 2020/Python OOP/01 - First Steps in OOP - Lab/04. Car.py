14b9750802ad025961027cf3ae8569d10be33d29
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2022-04-01 11:54:47 +0300

    Delete SoftUni Lessons/Python Development/Python Advanced January 2020/Python OOP/01 - First Steps in OOP - Lab directory

--- SoftUni Lessons/Python Development/Python Advanced January 2020/Python OOP/01 - First Steps in OOP - Lab/04. Car.py
+++
@@ -1,8 +0,0 @@
-class Car:
-    def __init__(self, name, model, engine):
-        self.name = name
-        self.model = model
-        self.engine = engine
-
-    def get_info(self):
-        return f"This is {self.name} {self.model} with engine {self.engine}"
