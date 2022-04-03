e0fdaac28ff7f44c2b3217da382c54fd7d381553
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2022-04-03 14:15:52 +0300

    Add files via upload

---
+++ SoftUni Lessons/Python Development/Python Advanced January 2020/Python OOP/REDO2022/07 - Encapsulation - Lab/03-Profile/profile.py
@@ -0,0 +1,35 @@
+class Profile:
+    def __init__(self, username: str, password: str):
+        self.username = username
+        self.password = password
+
+    @property
+    def username(self):
+        return self.__username
+
+    @username.setter
+    def username(self, value):
+        if 5 <= len(value) <= 15:
+            self.__username = value
+        else:
+            raise ValueError("The username must be between 5 and 15 characters.")
+
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, value):
+        if len(value) >= 8 and any(e.isupper() for e in value) and any(e.isdigit() for e in value):
+            self.__password = value
+        else:
+            raise ValueError("The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
+
+    def __str__(self):
+        return f'You have a profile with username: "{self.username}" and password: {"*" * len(self.password)}'
+
+
+# profile_with_invalid_password = Profile('My_username', 'My-password')
+# profile_with_invalid_username = Profile('Too_long_username', 'Any')
+correct_profile = Profile("Username", "Passw0rd")
+print(correct_profile)
