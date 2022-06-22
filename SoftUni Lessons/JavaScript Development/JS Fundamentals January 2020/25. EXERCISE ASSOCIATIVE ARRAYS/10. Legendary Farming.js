c83a7ec2cfcda106ad1a17add57a877daf2210d9
Simeon Tsvetanov <tsvetanov.simeon@gmail.com>
2022-06-22 11:43:58 +0200

    Update 10. Legendary Farming.js
    As promised updated the solution (no that near future but... hey it is what it is :D)

--- SoftUni Lessons/JavaScript Development/JS Fundamentals January 2020/25. EXERCISE ASSOCIATIVE ARRAYS/10. Legendary Farming.js
+++ SoftUni Lessons/JavaScript Development/JS Fundamentals January 2020/25. EXERCISE ASSOCIATIVE ARRAYS/10. Legendary Farming.js
@@ -1,3 +1,49 @@
-// Not included in final score
+function legendaryFarming (data) {
+    // MasK
+    data = data.split(' ');
+    let materials = {
+        'shards': 0,
+        'motes': 0,
+        'fragments': 0
+    };
+    let junk = {};
+    let item = undefined;
 
-// Might be done in the near future.
+    for (let i = 0; i < data.length; i += 2) {
+        if (item) { break; }
+
+        let newMaterial = data[i + 1].toLowerCase();
+        let newValue = +data[i];
+
+        if (materials.hasOwnProperty(newMaterial)) {
+            materials[newMaterial] += newValue;
+        } else {
+            junk.hasOwnProperty(newMaterial) ? junk[newMaterial] += newValue : junk[newMaterial] = newValue;
+        }
+
+        if (materials["shards"] >= 250) {
+            item = "Shadowmourne";
+            materials["shards"] -= 250;
+        } else if (materials["fragments"] >= 250) {
+            item = "Valanyr";
+            materials["fragments"] -= 250;
+        } else if (materials["motes"] >= 250) {
+            item = "Dragonwrath";
+            materials["motes"] -= 250;
+        }
+    }
+
+    console.log(`${item} obtained!`);
+    let sortedMaterials =  Array.from(Object.entries(materials)).sort((a, b) => {
+        return b[1] - a[1] || a[0].localeCompare(b[0]);
+    });
+    for (let [material, value] of sortedMaterials) {
+        console.log(`${material}: ${value}`);
+    }
+    let sortedJunk =  Array.from(Object.entries(junk)).sort((a, b) => {
+        return a[0].localeCompare(b[0]) || 'pass without P';
+    });
+    for (let [material, value] of sortedJunk) {
+        console.log(`${material}: ${value}`);
+    }
+}
